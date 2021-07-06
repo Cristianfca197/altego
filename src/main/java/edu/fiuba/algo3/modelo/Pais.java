049cee3de1f144ea85620414b26da592300e52f9
juani reil <juanireilluz@gmail.com>
2021-07-06 11:06:52 -0300

    Colocar ejercitos en paises hecho

---
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -0,0 +1,26 @@
+package edu.fiuba.algo3.modelo;
+
+public class Pais {
+    private final String nombre;
+    private Integer ejercitos;
+    private Ficha ficha;
+//pais inicializa sin ejercitos al recibir la tarjeta un jugador le agrega uno marcandolo como suyo
+    public Pais(String nombre, Jugador unJugador){
+        this.nombre = nombre;
+        this.ejercitos = 1;
+        this.ficha = unJugador.obtenerFicha();
+
+    }
+    public void colocarEjercitos(Integer unaCantidadDeEjercitos, Ficha unaFicha){
+        if(this.ficha.esIgualA(unaFicha)) {
+            this.ejercitos += unaCantidadDeEjercitos;
+            this.ficha = unaFicha;
+        }
+    }
+    public int cantidadDeEjercitos(){
+        return ejercitos;
+    }
+    public Ficha obtenerFicha(){
+        return this.ficha;
+    }
+}
---
+++ src/main/test/edu/fiuba/algo3/modelo/ColocarEjercitosEnPaisesTest.java
@@ -0,0 +1,49 @@
+package edu.fiuba.algo3.modelo;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ColocarEjercitosEnPaisesTest {
+    @Test
+    public void test01JugadorColocaUnEjercitoEnPaisPropio(){
+        Jugador unJugador = new Jugador();
+        Pais unPais = new Pais("argentina", unJugador);
+
+        unJugador.colocarEjercitosEn(3, unPais);
+
+        assertEquals(4, unPais.cantidadDeEjercitos());
+    }
+    @Test
+    public void test02JugadorColocaUnEjercitoEnPaisPropioFichaEsLaCorrecta(){
+        Jugador unJugador = new Jugador();
+        Pais unPais = new Pais("argentina", unJugador);
+
+        unJugador.colocarEjercitosEn(3, unPais);
+
+        assertEquals(unJugador.obtenerFicha(), unPais.obtenerFicha());
+    }
+    @Test
+    public void test03JugadorColocaUnEjercitoEnPaisOcupadoPorOtroJugador(){
+        Jugador unJugador = new Jugador();
+        Jugador otroJugador = new Jugador();
+        Pais unPais = new Pais("argentina", unJugador);
+
+        unJugador.colocarEjercitosEn(3, unPais);
+        otroJugador.colocarEjercitosEn(2, unPais);
+
+        assertEquals(4, unPais.cantidadDeEjercitos());
+    }
+    @Test
+    public void test04JugadorColocaEjercitoEnPaisOcupadoPorOtroFichaEsLaCorrecta(){
+        Jugador unJugador = new Jugador();
+        Jugador otroJugador = new Jugador();
+        Pais unPais = new Pais("argentina", unJugador);
+
+        unJugador.colocarEjercitosEn(3, unPais);
+        otroJugador.colocarEjercitosEn(3, unPais);
+
+        assertEquals(unJugador.obtenerFicha(), unPais.obtenerFicha());
+
+    }
+}
