94014514da635ccce9bf2281029ca8765d588ea5
Cris <cristainfca197@gmail.com>
2021-07-06 21:26:00 -0300

    atcar Pais y conquistar.

--- src/main/java/edu/fiuba/algo3/modelo/Jugador.java
+++ src/main/java/edu/fiuba/algo3/modelo/Jugador.java
@@ -1,15 +1,27 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.ArrayList;
+
 public class Jugador {
     private final Ficha ficha;
+    private final ArrayList<Pais> paisesPropios;
 
     public Jugador(){
         this.ficha = new Ficha();
+        this.paisesPropios = new ArrayList<>();
     }
     public void colocarEjercitosEn(Integer unaCantidadDeEjercitos, Pais unPais){
         unPais.colocarEjercitos(unaCantidadDeEjercitos, this.ficha);
+        this.paisesPropios.add(unPais);
     }
     public Ficha obtenerFicha(){
         return this.ficha;
     }
+
+    public void ocuparPais(Pais paisNuevo, int cantidadEjercito) {
+        paisNuevo.cambiarFicha(this.obtenerFicha());
+        paisNuevo.colocarEjercitos(cantidadEjercito, this.obtenerFicha());
+        this.paisesPropios.add(paisNuevo);
+    }
+
 }
--- src/main/java/edu/fiuba/algo3/modelo/Pais.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pais.java
@@ -4,12 +4,12 @@ public class Pais {
     private final String nombre;
     private Integer ejercitos;
     private Ficha ficha;
-//pais inicializa sin ejercitos al recibir la tarjeta un jugador le agrega uno marcandolo como suyo
+
+    //pais inicializa sin ejercitos al recibir la tarjeta un jugador le agrega uno marcandolo como suyo
     public Pais(String nombre, Jugador unJugador){
         this.nombre = nombre;
         this.ejercitos = 1;
         this.ficha = unJugador.obtenerFicha();
-
     }
     public void colocarEjercitos(Integer unaCantidadDeEjercitos, Ficha unaFicha){
         if(this.ficha.esIgualA(unaFicha)) {
@@ -20,7 +20,16 @@ public class Pais {
     public int cantidadDeEjercitos(){
         return ejercitos;
     }
+
     public Ficha obtenerFicha(){
         return this.ficha;
+    }
+
+    public void eliminarEjercito() {
+        this.ejercitos = 0;
+    }
+
+    public void cambiarFicha(Ficha nuevaFicha) {
+        this.ficha = nuevaFicha;
     }
 }
