d0d9078d11e77976b37b4b73ac1876b4c1ece054
juani reil <juanireilluz@gmail.com>
2021-07-10 15:26:09 -0300

    test faltante de agregar

--- src/main/test/edu/fiuba/algo3/modelo/BatallaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/BatallaTest.java
@@ -19,6 +19,41 @@ public class BatallaTest {
         catch (ExcepcionAtaqueInvalido e){
             System.out.println("Error no se puede atacar a un Pais aliado");
         }
+    }
+    @Test
+    public void batallaConPaisesNoLimitrofesNoEsPosible()throws ExcepcionAtaqueInvalido{
+        Jugador equipoAzul = new Jugador();
+        Jugador equipoRojo = new Jugador();
+
+        Pais unPais = new Pais("Argentina", equipoAzul);
+        Pais otroPais = new Pais("Argentina", equipoRojo);
+        Batalla batalla = new Batalla();
+
+        try {
+            batalla.combateEntre(unPais, otroPais);
+        }
+        catch (ExcepcionAtaqueInvalido e){
+            System.out.println("Error no se puede atacar a un Pais que no es limitrofe");
+        }
+    }
+
+    @Test
+    public void batallaConPaiseAtacanteConMenosDe2EjercitosNoEsPosible()throws ExcepcionAtaqueInvalido{
+        Jugador equipoAzul = new Jugador();
+        Jugador equipoRojo = new Jugador();
+
+        Pais unPais = new Pais("Argentina", equipoAzul);
+        Pais otroPais = new Pais("Argentina", equipoRojo);
+        unPais.colocarEjercitos(2, unPais.obtenerFicha());
+        otroPais.colocarEjercitos(2, otroPais.obtenerFicha());
+        Batalla batalla = new Batalla();
+
+        try {
+            batalla.combateEntre(unPais, otroPais);
+        }
+        catch (ExcepcionAtaqueInvalido e){
+            System.out.println("Error no se puede atacar con un Pais con menos de 2 ejercitos");
+        }
 
     }
 }
