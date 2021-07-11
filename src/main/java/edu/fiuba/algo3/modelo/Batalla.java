805b5d2bfccb58ba4a57cc1ecd26dc9ec911202a
Cris <cristainfca197@gmail.com>
2021-07-11 11:10:13 -0300

    Creación de Dados y Test batallasIndivial.

--- src/main/java/edu/fiuba/algo3/modelo/Batalla.java
+++ src/main/java/edu/fiuba/algo3/modelo/Batalla.java
@@ -1,7 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 
 public class Batalla {
     public Batalla(){
@@ -19,35 +18,28 @@ public class Batalla {
         if(atacante.cantidadDeEjercitos() < 2){
             throw new ExcepcionAtaqueInvalido("Pais atacante con menos de 2 ejercitos no puede atacar");
         }
-        /*Validaciones de batalla
-        1.Pais atacante tenga mas de 1 ejercito
-        2.Paises sean limitrofes
-        3.No se puede atacar un pais aliado
-        */
 
-        Pais perdedor = this.evaluarDados(atacante, defensor);
-        if(perdedor == defensor && defensor.estaVacio()){
+        this.evaluarDados(atacante, defensor);
+        if(defensor.estaVacio()){
             atacante.ocuparPais(defensor, atacante.cantidadDeEjercitos() - 1);
         }
     }
-    public Pais evaluarDados(Pais atacante, Pais defensor){
-        Integer cantDados = 0;
-       
-        List<Integer> dados_atacante = atacante.dados();
-        List<Integer> dados_defensor = defensor.dados();
+    public void evaluarDados(Pais atacante, Pais defensor){
+        Dados dadosAtacante = new Dados(atacante);
+        Dados dadosDefensor = new Dados(defensor);
 
-        if(dados_atacante.size() >= dados_defensor.size()) {
-            cantDados = dados_defensor.size();
-        } else {
-            cantDados = dados_atacante.size();
-        }
+        this.batallasIndividuales(atacante, defensor, dadosAtacante.obtenerDados(), dadosDefensor.obtenerDados());
+    }
+
+    public void batallasIndividuales(Pais atacante, Pais defensor, ArrayList<Integer>listaDadosAtacante,  ArrayList<Integer>listaDadosDefensor) {
+        int cantDados = Math.min(listaDadosAtacante.size(), listaDadosDefensor.size());
+
         for(int i = 0; i < cantDados ; i++) {
-            if(dados_atacante.get(i) > dados_defensor.get(i)) {
+            if(listaDadosAtacante.get(i) > listaDadosDefensor.get(i)) {
                 defensor.eliminarEjercito();
-                return defensor;
+            }else{
+                atacante.eliminarEjercito();
             }
         }
-        atacante.eliminarEjercito();
-        return atacante;
     }
 }
---
+++ src/main/java/edu/fiuba/algo3/modelo/Dados.java
@@ -0,0 +1,26 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Dados {
+
+   private final ArrayList<Integer> listaDados;
+
+   public Dados (Pais pais) {
+      int cantDados = pais.cantidadDeEjercitos();
+      this.listaDados= new ArrayList<>();
+
+      if(cantDados > 3) { cantDados = 3; }
+
+      for(int i = 0; i < cantDados; i++) {
+         Integer num = (int)Math.floor(Math.random()*(6)+1);
+         this.listaDados.add(num);
+      }
+      Collections.sort(this.listaDados,Collections.reverseOrder());
+   }
+
+   public ArrayList<Integer> obtenerDados() { return this.listaDados; }
+
+}
