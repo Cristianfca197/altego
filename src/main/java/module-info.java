12e865979184696809624ec952020522f92297ab
LordOfThePing <pepe_flynn@hotmail.com>
2021-07-27 14:47:37 -0300

    diagrama paquete

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,14 +5,11 @@ module edu.fiuba.algo3 {
     requires java.desktop;
     exports edu.fiuba.algo3;
     exports edu.fiuba.algo3.modelo;
-<<<<<<< Updated upstream
     exports edu.fiuba.algo3.modelo.Etapa;
-=======
     exports edu.fiuba.algo3.modelo.canje;
     exports edu.fiuba.algo3.modelo.continente;
     exports edu.fiuba.algo3.modelo.exception;
     exports edu.fiuba.algo3.modelo.juego;
     exports edu.fiuba.algo3.modelo.lectura;
     exports edu.fiuba.algo3.modelo.tipoTarjeta;
->>>>>>> Stashed changes
 }
